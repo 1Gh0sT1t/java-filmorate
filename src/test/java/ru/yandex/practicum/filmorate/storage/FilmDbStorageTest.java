d068ea2932b841e0ec014858696a2dd89b6c8d0d
Marat <marat_beknazarya@mail.ru>
2026-03-09 11:11:49 +0300

    удалил неиспользуемый импорт, для прохождения чекстайла

--- src/test/java/ru/yandex/practicum/filmorate/storage/FilmDbStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/storage/FilmDbStorageTest.java
@@ -13,7 +13,6 @@ import ru.yandex.practicum.filmorate.model.User;
 
 import java.time.LocalDate;
 import java.util.Collection;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
